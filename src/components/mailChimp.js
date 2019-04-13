9e935c13066a629f442d6eac55fbd83b6549bf30
Dan Koe <thedankoe@gmail.com>
2019-04-13 10:08:18 -0700

    success and error messages

--- src/components/mailChimp.js
+++ src/components/mailChimp.js
@@ -19,8 +19,7 @@ const InputStyle = styled.input`
   font-size: 1.8rem;
   font-family: inherit;
   color: ${props => props.theme.lightestGrey};
-  text-transform: uppercase;
-  letter-spacing: 0.5rem;
+  letter-spacing: 0.3rem;
   padding: 1.5rem 2rem;
   border-radius: 3px;
   background-color: rgba(255, 255, 255, 0.2);
@@ -51,6 +50,7 @@ const ButtonStyle = styled.button`
   color: ${props => props.theme.lightestGrey};
   text-transform: uppercase;
   letter-spacing: 0.5rem;
+  cursor: pointer;
   transition: all ease 0.3s;
 
   &:hover {
@@ -59,10 +59,50 @@ const ButtonStyle = styled.button`
   }
 `
 
+const SuccessMessage = styled.p`
+  margin-top: 1.5rem;
+  padding: 1rem 1.5rem;
+  background: rgba(85, 197, 122, 0.8);
+  border-radius: 3px;
+  font-size: 1.8rem;
+  color: ${props => props.theme.lightestGrey};
+  letter-spacing: 0.3rem;
+`
+
+const ErrorMessage = styled.p`
+  margin-top: 1.5rem;
+  padding: 1rem 1.5rem;
+  background: rgba(255, 119, 48, 0.8);
+  border-radius: 3px;
+  font-size: 1.8rem;
+  color: ${props => props.theme.lightestGrey};
+  letter-spacing: 0.3rem;
+`
+
+const formSubmit = value => {
+  if (value.result === 'success') {
+    return (
+      <SuccessMessage>
+        Success! You have successfully subscribed. Check your email for
+        confirmation.
+      </SuccessMessage>
+    )
+  }
+
+  if (value.result === 'error') {
+    return (
+      <ErrorMessage>
+        Error: You may have already subscribed. If you haven't, contact me.
+      </ErrorMessage>
+    )
+  }
+}
+
 const Subscribe = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const [subResult, setSubResult] = useState('')
 
   return (
     <form
@@ -72,7 +112,10 @@ const Subscribe = () => {
           FNAME: firstName,
           LNAME: lastName,
         })
-        console.log(result)
+        setSubResult(result)
+        setFirstName('')
+        setLastName('')
+        setEmail('')
       }}
     >
       <InputContainer>
@@ -95,6 +138,7 @@ const Subscribe = () => {
           placeholder="Email"
         />
         <ButtonStyle type="submit">Submit</ButtonStyle>
+        {formSubmit(subResult, setSubResult.msg)}
       </InputContainer>
     </form>
   )
